ee18f1d24ed8c1d8240801735ffc8b6f53c320e3
Zsofia <oroszi.zsofi@gmail.com>
2020-04-26 18:17:51 +0200

    add PaletteList and Links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Router, Switch, Route } from 'react-router-dom';
 import Palette from './Palette';
+import PaletteList from './PaletteList';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
@@ -13,7 +14,7 @@ class App extends Component {
   render(){
     return (
       <Switch>
-        <Route exact path="/" render={() => <h1>PALETTE GOES HERE</h1>} />
+        <Route exact path="/" render={() => <PaletteList palettes={seedColors} />} />
         <Route exact path="/palette/:id" render={(routeProps) => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))}/>} />
       </Switch>
       // <div>
