c6628d4204bbdb64376393aaa3579c3b6d107535
Zsofia <oroszi.zsofi@gmail.com>
2020-04-26 18:01:12 +0200

    add function for finding palettes by route

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
 class App extends Component {
+  findPalette(id){
+    return seedColors.find(function(palette){
+      return palette.id === id
+    });
+  }
   render(){
     return (
       <Switch>
         <Route exact path="/" render={() => <h1>PALETTE GOES HERE</h1>} />
-        <Route exact path="/palette/:id" render={() => <h1>INDIVIDUAL PALETTE GOES HERE</h1>} />
+        <Route exact path="/palette/:id" render={(routeProps) => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))}/>} />
       </Switch>
       // <div>
       //   <Palette palette={generatePalette(seedColors[4])} />
