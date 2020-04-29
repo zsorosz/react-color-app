3b58ba4d946f17e89c9f5b61c29f73ff75ec8093
Zsofia <oroszi.zsofi@gmail.com>
2020-04-29 18:07:10 +0200

    Refactor ColorBox styles

--- src/Palette.js
+++ src/Palette.js
@@ -27,7 +27,7 @@ class Palette extends Component {
                 key={color.id} 
                 id={color.id} 
                 paletteId={id}
-                showLink
+                showingFullPalette
             />
         ));
         return(
