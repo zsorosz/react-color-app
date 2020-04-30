741eaf8b4465a482dabdfb1a8e9f8f06faa0d6bd
Zsofia <oroszi.zsofi@gmail.com>
2020-04-30 16:39:32 +0200

    add NewPaletteForm component and Link

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -15,23 +15,27 @@ export default {
     },
     nav: {
         display: "flex",
+        width: "100%",
         justifyContent: "space-between",
+        alignItems: "center",
         color: "white",
-        "& h1": {
-            margin: "1rem"
+        "& a": {
+            color: "white",
+            margin: 0
         }
     },
     palettes: {
         boxSizing: "border-box",
         width: "100%",
         display: "flex",
+        justifyContent: "space-between",
         flexWrap: "wrap",
         // gridTemplateColumns: "repeat(3, 30%)",
         // gridGap: "5%"
     },
     miniPaletteContainer: {
-        width: "25%",
+        width: "30%",
         height: "30%",
-        margin: "1rem"
+        // margin: "1rem"
     }
 }
