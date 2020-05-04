3a663a70a6a85a615fbf7dac1c8c61d939cfb36c
Zsofia <oroszi.zsofi@gmail.com>
2020-05-04 11:27:07 +0200

    add Delete function to PaletteForm

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -35,12 +35,12 @@ const styles = {
 };
 
 function DragabbleColorBox(props) {
-    const { classes } = props;
+    const { classes, handleClick, name, color } = props;
     return(
-        <div className={classes.root} style={{ backgroundColor: props.color}}>
+        <div className={classes.root} style={{ backgroundColor: color}}>
             <div className={classes.boxContent}>
-                <span>{props.name}</span>
-                <DeleteIcon className={classes.deleteIcon} />
+                <span>{name}</span>
+                <DeleteIcon className={classes.deleteIcon} onClick={handleClick} />
             </div>
         </div>
     )
--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -143,6 +143,12 @@ class NewPaletteForm extends Component {
             }
           this.props.savePalette(newPalette);
           this.props.history.push("/");
+      };
+
+      removeColor(colorName){
+          this.setState({
+              colors: this.state.colors.filter(color => color.name !== colorName)
+          });
       }
     
       render() {
@@ -233,7 +239,12 @@ class NewPaletteForm extends Component {
             >
                 <div className={classes.drawerHeader} />
                 {this.state.colors.map(color => (
-                    <DraggableColorBox color={color.color} name={color.name} />
+                    <DraggableColorBox 
+                        key={color.name}
+                        color={color.color} 
+                        name={color.name} 
+                        handleClick= {() => this.removeColor(color.name)}
+                    />
                 ))}
             </main>
           </div>
