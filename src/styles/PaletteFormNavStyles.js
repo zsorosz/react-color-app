7fd21cfd7ee272c963b38ecad0e02216f51dcd60
Zsofia <oroszi.zsofi@gmail.com>
2020-05-05 12:44:07 +0200

    tweak Form styles

--- src/styles/PaletteFormNavStyles.js
+++ src/styles/PaletteFormNavStyles.js
@@ -6,37 +6,40 @@ const styles = theme => ({
     root: {
         display: "flex"
     },
-  appBar: {
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    height: "64px"
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginLeft: 12,
-    marginRight: 20,
-  },
-  navBtns: {
-    marginRight: "1.5rem",
-    "& a": {
-        textDecoration: "none"
+    hide: {
+      display: 'none',
+    },
+    appBar: {
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      height: "64px"
+    },
+    appBarShift: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginLeft: 12,
+      marginRight: 20,
+    },
+    navBtns: {
+      marginRight: "1.5rem",
+      "& a": {
+          textDecoration: "none"
+      }
+    },
+    button: {
+        margin: "0 0.5rem"
     }
-  },
-  button: {
-      margin: "0 0.5rem"
-  }
 });
 
 export default styles;
